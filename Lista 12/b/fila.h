692fcd76845e0e14e55dce025a149453afdcb57c
Vinícius Lima Carvalho <127750264+ViniciusLimaCarvalho@users.noreply.github.com>
2023-10-18 18:10:36 -0300

    exercicio b lista 12

---
+++ Lista 12/b/fila.h
@@ -0,0 +1,22 @@
+#ifndef FILA_H_INCLUDED
+#define FILA_H_INCLUDED
+
+#define MAX 100
+
+typedef struct fila Fila;
+
+Fila *criar();
+void limpar(Fila *f);
+
+int inserir(Fila *f, int it);
+int remover(Fila *f,int *it);
+
+int consultar(Fila *f, int *it);
+int tamanho(Fila *f);
+
+int filaVazia(Fila *f);
+int filaCheia(Fila *f);
+
+int intercalarFilas (Fila *f, Fila *r, Fila *intercalada);
+
+#endif // FILA_H_INCLUDED
